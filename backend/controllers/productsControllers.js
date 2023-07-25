e738d6b504d33ebb47ed29d74887c53856c7f4fc
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-07-25 13:32:52 -0600

    product CRUD working

--- backend/controllers/productsControllers.js
+++ backend/controllers/productsControllers.js
@@ -1,16 +1,9 @@
 const asyncHandler = require('express-async-handler')
 const Product = require('../models/productsModel')
 
-
 const getAllProducts = asyncHandler(async (req, res) => {
     const products = await Product.find()
     res.status(200).json(products)
-})
-
-const getProduct = asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    // const product = await Product.find({ _id: req.params.id })
-    res.status(200).json(product)
 })
 
 const createProduct = asyncHandler(async (req, res) => {
@@ -31,17 +24,34 @@ const createProduct = asyncHandler(async (req, res) => {
     res.status(201).json(newProduct)
 })
 
+const getProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if(!product){
+        res.status(400)
+        throw new Error("Product doesn't exists")
+    }
+    // const product = await Product.find({ _id: req.params.id })
+    res.status(200).json(product)
+})
+
 const updateProduct = asyncHandler(async (req, res) => {
     const productToUpdate = await Product.findById(req.params.id)
     if(!productToUpdate){
         res.status(400)
         throw new Error("Product doesn't exists")
     }
-    res.status(200).json({ message: `Update Product with id: ${req.params.id}`})
+    const updatedProduct = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    res.status(200).json(updatedProduct)
 })
 
 const deleteProduct = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete Product with id: ${req.params.id}`})
+    const productToDelete = await Product.findById(req.params.id)
+    if(!productToDelete){
+        res.status(400)
+        throw new Error("Product doesn't exists")
+    }
+    await productToDelete.deleteOne()
+    res.status(200).json({ id: req.params.id})
 })
 
 
