0f01c86df43bfe1f32e94a64038f39c25c62b20d
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-07-24 21:48:21 -0600

    error middleware and asynchandler

--- backend/controllers/productsControllers.js
+++ backend/controllers/productsControllers.js
@@ -1,22 +1,29 @@
-const getAllProducts = async (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+
+const getAllProducts = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get All Products'})
-}
+})
 
-const getProduct = async (req, res) => {
+const getProduct = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Get Product with id: ${req.params.id}`})
-}
+})
 
-const createProduct = async (req, res) => {
+const createProduct = asyncHandler(async (req, res) => {
+    if(!req.body.title || !req.body.description || !req.body.price || !req.body.category) {
+        res.status(400)
+        throw new Error('Por favor completa la informacion del producto')
+    }
     res.status(201).json({ message: 'Create Product'})
-}
+})
 
-const updateProduct = async (req, res) => {
+const updateProduct = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update Product with id: ${req.params.id}`})
-}
+})
 
-const deleteProduct = async (req, res) => {
+const deleteProduct = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete Product with id: ${req.params.id}`})
-}
+})
 
 
 module.exports = {
--- backend/routes/productsRoutes.js
+++ backend/routes/productsRoutes.js
@@ -2,15 +2,14 @@ const express = require('express')
 const router = express.Router()
 const { getAllProducts, getProduct, createProduct, updateProduct, deleteProduct } = require('../controllers/productsControllers')
 
-router.get('/', getAllProducts)
+router.route('/').get(getAllProducts).post(createProduct)
+// router.get('/', getAllProducts)
+// router.post('/', createProduct)
 
-router.get('/:id', getProduct)
-
-router.post('/', createProduct)
-
-router.put('/:id', updateProduct)
-
-router.delete('/:id', deleteProduct)
+router.route('/:id').get(getProduct).put(updateProduct).delete(deleteProduct)
+// router.get('/:id', getProduct)
+// router.put('/:id', updateProduct)
+// router.delete('/:id', deleteProduct)
 
 
 module.exports = router
