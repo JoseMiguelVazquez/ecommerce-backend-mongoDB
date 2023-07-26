7ee9c6e74cd0a8b3f7967bd134e408864330af43
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-07-26 10:03:36 -0600

    register and login controllers

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -15,21 +15,64 @@ const registerUser = asyncHandler(async (req, res) => {
     const userExists = await User.findOne({email})
     if(userExists) {
         res.status(400)
-        throw new Error('Ese usuario ya existe')
+        throw new Error('User already exists')
     }
 
     const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
 
-    res.json({message: 'Usuario creado'})
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user){
+        res.status(201)
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    }
+    else {
+        res.status(400)
+        throw new Error('Register could not be saved')
+    }
 })
 
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({message: 'Usuario logueado'})
+    const {email, password} = req.body
+
+    if(!email || !password){
+        res.status(400)
+        throw new Error('Please provide both email and password')
+    }
+
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    }else{
+        res.status(401)
+        throw new Error('Incorrect email or password')
+    }
 })
 
 const getUserData = asyncHandler(async (req, res) => {
     res.json({message: 'User Data'})
 })
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '60m'
+    })
+}
 
 module.exports = {
     registerUser,
